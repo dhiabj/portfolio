dfa65e441afc5b86008e058bc34de5e417328374
Dhia Bejaoui <dhia.bejaoui.db@gmail.com>
2024-12-16 16:52:48 +0100

    minor changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,13 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Dhia | Portfolio',
-  description: "Dhia's portfolio website",
+  title: {
+    template: '%s | Dhia Bejaoui Portfolio',
+    default: 'Dhia Bejaoui Portfolio',
+  },
+  description: 'Welcome to Dhia Bejaoui\'s portfolio. Explore projects, skills, and experiences in full-stack web development, including JavaScript, TypeScript, React, Redux, Node.js, Express, MongoDB, HTML, CSS, and Git.',
+  metadataBase: new URL('https://www.dhiabejaoui.com/'),
+  keywords: ['Next.js', 'React', 'JavaScript', 'TypeScript', 'JAVA', 'HTML5', 'CSS3', 'SASS', 'PHP', 'SQL', 'Symfony', 'Redux', 'React Native', 'TailwindCSS', 'Angular', 'Firebase', 'Spring Boot', 'MongoDB', 'Express.js', 'Node.js', 'Git'],
 };
 
 export default function RootLayout({ children }) {
