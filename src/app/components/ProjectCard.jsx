6aa4a8e7823cb651e21f1051bcec4b1f6e9c7f87
Dhia Bejaoui <dhia.bejaoui.db@gmail.com>
2024-12-20 18:52:59 +0100

    add new project

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CodeBracketIcon } from '@heroicons/react/24/outline';
+import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const getRandomColor = () => {
@@ -18,27 +18,40 @@ const getRandomColor = () => {
   return colors[randomIndex];
 };
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, skills }) => {
+const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  skills,
+  previewUrl,
+}) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
+        style={{ background: `url(${imgUrl})`, backgroundSize: 'cover' }}>
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-            target="_blank"
-          >
+            target="_blank">
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
+          {previewUrl && (
+            <Link
+              href={previewUrl}
+              target="_blank"
+              className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
+              <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            </Link>
+          )}
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE] mb-2">{description}</p>
-        <div className='flex gap-2 flex-wrap'>
+        <div className="flex gap-2 flex-wrap">
           {skills.map((skill, index) => (
             <span key={index} className={getRandomColor()}>
               #{skill}
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,39 +1,77 @@
 'use client';
 import React, { useState, useRef } from 'react';
 import ProjectCard from './ProjectCard';
-import ProjectTag from './ProjectTag';
 import { motion, useInView } from 'framer-motion';
 
 const projectsData = [
   {
     id: 1,
-    title: 'Campside',
+    title: 'Weather App',
     description:
-      'Campside is a social network for those who enjoy camping. The platform attempts to make sharing and finding camping locations easier. On the site, users can interact with each other and exchange experiences. Campside offers a social hub for campers, encouraging a community built around outdoor adventures.',
+      'A simple single-page responsive weather app developed using Nextjs, TypeScript, and Tailwind CSS that displays current weather information for any location worldwide. The app utilizes the OpenWeather API for weather data and features dynamic backgrounds, search history, and unit conversion.',
     image: '/images/projects/1.png',
     tag: ['All', 'Web'],
-    gitUrl: 'https://github.com/dhiabj/campside.git',
-    skills:['Mongodb','Express.js','Node.js','React.js','JavaScript','Bootstrap','Git']
+    gitUrl: 'https://github.com/dhiabj/weather-app.git',
+    previewUrl: 'https://weather-app-beryl-seven-94.vercel.app/',
+    skills: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Vercel', 'Git'],
   },
   {
     id: 2,
-    title: 'Eco-Volunteers',
+    title: 'Campside',
     description:
-      'Ecovolunteers is a social network dedicated to charities, allowing volunteers to communicate, to meet and to share their accomplishments and experiences. Features including live chat, sharing moments with reactions, donating, and managing clubs and various events. It also incorporated advanced functionalities like an AI chatbot for interaction and a speech recognition bot for hands-free assistance.',
+      'Campside is a social network for those who enjoy camping. The platform attempts to make sharing and finding camping locations easier. On the site, users can interact with each other and exchange experiences. Campside offers a social hub for campers, encouraging a community built around outdoor adventures.',
     image: '/images/projects/2.png',
     tag: ['All', 'Web'],
-    gitUrl: 'https://github.com/dhiabj/Ecovolunteers.git',
-    skills:['Mongodb','Express.js','Node.js','React.js','JavaScript','Bootstrap','Redux','Socket.IO','Material UI','Git']
+    gitUrl: 'https://github.com/dhiabj/campside.git',
+    skills: [
+      'Mongodb',
+      'Express.js',
+      'Node.js',
+      'React.js',
+      'JavaScript',
+      'Bootstrap',
+      'Git',
+    ],
   },
   {
     id: 3,
+    title: 'Eco-Volunteers',
+    description:
+      'Ecovolunteers is a social network dedicated to charities, allowing volunteers to communicate, to meet and to share their accomplishments and experiences. Features including live chat, sharing moments with reactions, donating, and managing clubs and various events. It also incorporated advanced functionalities like an AI chatbot for interaction and a speech recognition bot for hands-free assistance.',
+    image: '/images/projects/3.png',
+    tag: ['All', 'Web'],
+    gitUrl: 'https://github.com/dhiabj/Ecovolunteers.git',
+    skills: [
+      'Mongodb',
+      'Express.js',
+      'Node.js',
+      'React.js',
+      'JavaScript',
+      'Bootstrap',
+      'Redux',
+      'Socket.IO',
+      'Material UI',
+      'Git',
+    ],
+  },
+  {
+    id: 4,
     title: '4You',
     description:
-      "4You is a Web, Desktop and mobile application for a restaurant that offers customers a delivery service and customization for any type of diet. Features including the ability for customers to rate dishes and leave detailed reviews.",
+      '4You is a Web, Desktop and mobile application for a restaurant that offers customers a delivery service and customization for any type of diet. Features including the ability for customers to rate dishes and leave detailed reviews.',
     image: '/images/projects/4.png',
     tag: ['All', 'Web'],
     gitUrl: 'https://github.com/dhiabj/4You.git',
-    skills:['JavaFx','Java','Symfony','Php','Twig','Bootstrap','Codename One','Git']
+    skills: [
+      'JavaFx',
+      'Java',
+      'Symfony',
+      'Php',
+      'Twig',
+      'Bootstrap',
+      'Codename One',
+      'Git',
+    ],
   },
 ];
 
@@ -76,6 +114,7 @@ const ProjectsSection = () => {
               imgUrl={project.image}
               gitUrl={project.gitUrl}
               skills={project.skills}
+              previewUrl={project.previewUrl}
             />
           </motion.li>
         ))}
