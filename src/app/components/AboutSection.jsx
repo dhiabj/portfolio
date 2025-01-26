30542a7271bedbc75ddfbb8aeb71587d56055a35
Dhia Bejaoui <dhia.bejaoui.db@gmail.com>
2025-01-26 16:38:06 +0100

    minor changes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -134,12 +134,12 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, TypeScript, React, Redux, Node.js, Express,
-            MongoDB, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a full-stack developer with a passion for creating interactive
+            and responsive web applications. I have experience working with
+            JavaScript, TypeScript, React, Redux, Node.js, Symfony, MongoDB,
+            HTML, CSS, Tailwind CSS, and Git. I am a quick learner, always
+            looking to expand my knowledge and skill set. As a team player, I am
+            excited to collaborate with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
