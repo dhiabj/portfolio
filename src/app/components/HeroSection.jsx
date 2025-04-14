6df16cb4fb7a7bc7d38b8b76206bba252b7af7ca
dhiabj <dhia.bejaoui.db@gmail.com>
2025-04-14 16:55:30 +0100

    update portfolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1BKsK5ujuPc8u6RnWXWiNTJha2Nvz1_8O/view?usp=sharing"
+              href="https://drive.google.com/file/d/1onog3idHSterp4s0RGuJI8HGWl5NrH37/view?usp=sharing"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
